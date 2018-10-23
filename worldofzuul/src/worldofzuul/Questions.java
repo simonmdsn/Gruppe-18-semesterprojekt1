f7a6835657c56cf5cc97184ecd7c748c458eeec4
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2018-10-23 15:59:43 +0200

    Questions class
    questions added
    check answer added
    
    Co-Authored-By: Benjamin Hamborg Clement Klerens <benjaminklerens@users.noreply.github.com>
    Co-Authored-By: turtlepriest <turtlepriest@users.noreply.github.com>

--- worldofzuul/src/worldofzuul/Questions.java
+++ worldofzuul/src/worldofzuul/Questions.java
@@ -1,12 +1,45 @@
 package worldofzuul;
 
+import java.util.Scanner;
+
 public class Questions {
     
+    //Attributes
+    private String answer;
+    private boolean checkAnswer = false;
+  
+    
+    //Scanner objekt
+    private Scanner readAnswer = new Scanner(System.in);
     
     public void caliQuestions(){
+        
         System.out.println("\nHow high is California ranked in population size?");
         System.out.println("A: First    B: Third    C: Eight");
-            
+        checkAnswer();
+        if(checkAnswer == true){
+           if(answer.equals("a") || answer.equals("A")){
+               System.out.println("Correct, California is ranked first in population size.");
+           } else {
+               System.out.println("Incorrect, California is actually ranked first in population size");
+           }
+           readAnswer.close();
+        }
+        
+    }
+        
+    public void checkAnswer(){
+        
+        while(!checkAnswer) {
+        answer = readAnswer.next();
+        if(answer.equals("a") || answer.equals("b") || answer.equals("c")){
+            checkAnswer = true;
+        } else{
+        System.out.println("Incorrect input, try again.");
+                }
+        }
+        
+        
             
 }
     
