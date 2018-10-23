a2c878ec74a295776ad348e635de6c2250ff596d
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2018-10-23 15:07:58 +0200

    Question class
    Question class added

---
+++ worldofzuul/src/worldofzuul/Questions.java
@@ -0,0 +1,13 @@
+package worldofzuul;
+
+public class Questions {
+    
+    
+    public void caliQuestions(){
+        System.out.println("\nHow high is California ranked in population size?");
+        System.out.println("A: First    B: Third    C: Eight");
+            
+            
+}
+    
+}
