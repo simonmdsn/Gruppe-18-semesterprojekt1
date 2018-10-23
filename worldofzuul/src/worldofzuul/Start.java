767cde13e4ea867963f11bb70fd1727308bc3188
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2018-10-23 15:12:29 +0200

    //test
    test

--- worldofzuul/src/worldofzuul/Start.java
+++ worldofzuul/src/worldofzuul/Start.java
@@ -5,9 +5,9 @@ public class Start {
     public static void main(String[] args) {
             
         //Creates an instance of 'Game' and calls the method 'play' 
-        Game game = new Game(); 
+        Game game = new Game();
         game.play();
-       
+        //test
              
 
     }
